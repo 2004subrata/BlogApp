c77331319f5cac815eb9d07f8740f1c866bade96
Subrata Mahato <104990409+2004subrata@users.noreply.github.com>
2025-12-04 02:04:56 +0530

    Add sidebar functionalities of dashboard

--- main.js
+++ main.js
@@ -17,3 +17,28 @@ const closeNav = ()=> {
 
 openNavBtn.addEventListener('click', openNav);
 closeNavBtn.addEventListener('click', closeNav);
+
+
+
+
+const sidebar = document.querySelector('aside');
+const showSidebarBtn = document.querySelector('#show_sidebar-btn');
+const hideSidebarBtn = document.querySelector('#hide_sidebar-btn');
+
+
+//show sidebar on small devices
+const showSidebar = () => {
+    sidebar.style.left = "0";
+    showSidebarBtn.style.display = "none";
+    hideSidebarBtn.style.display = "inline-block"
+}
+
+//Hide sidebar on small devices
+const hideSidebar = () => {
+    sidebar.style.left = "-100%";
+    showSidebarBtn.style.display = "inline-block";
+    hideSidebarBtn.style.display = "none"
+}
+
+showSidebarBtn.addEventListener('click', showSidebar);
+hideSidebarBtn.addEventListener('click', hideSidebar);
