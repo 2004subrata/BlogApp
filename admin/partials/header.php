f0c38cddae6176aca99f0e25e6fca8d20f4dfcd2
Subrata Mahato <104990409+2004subrata@users.noreply.github.com>
2025-12-12 19:37:50 +0530

    require all heading from 'ROOT_URl'header.php

--- admin/partials/header.php
+++ admin/partials/header.php
@@ -1,56 +1,9 @@
 <?php
-require 'config/database.php';
+require '../partials/header.php';
 
+//check login status 
+if(!isset($_SESSION['user-id'])) {
+    header('location: ' . ROOT_URl . 'signin.php');
+    die();
+}
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Creative Nova : The Blog Website</title>
-
-    <!-- custom css  -->
-    <link rel="stylesheet" href="<?= ROOT_URl ?>css/style.css">
-
-    <!-- ICONSCOUT CDN  -->
-     <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.2.0/css/line.css">
-
-     <!-- FONT AWESOME CDN -->
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-
-      <!-- Google Fonts (Montserrat) -->
-       <link rel="preconnect" href="https://fonts.googleapis.com">
-       <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-       <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
-</head>
-<body>
-    <nav>
-        <div class="container nav_container">
-            <a href="<?= ROOT_URl ?>" class="nav_logo">Creative Nova</a>
-            <ul class="nav_items">
-                <li><a href="<?= ROOT_URl ?>blog.php">Blog</a></li>
-                <li><a href="<?= ROOT_URl ?>about.php">About</a></li>
-                <li><a href="<?= ROOT_URl ?>services.php">Services</a></li>
-                <li><a href="<?= ROOT_URl ?>contact.php">Contact</a></li>
-                <!-- <li><a href="<?= ROOT_URl ?>signin.php">Signin</a></li> -->
-                <li class="nav_profile">
-                    
-                        <div class="avatar">
-                            <img src="./images/avatar1.jpg" alt="">
-                        </div>
-                        <ul>
-                            <li><a href="<?= ROOT_URl ?>admin/index.php">Dashboard</a></li>
-                            <li><a href="<?= ROOT_URl ?>logout.php">Dashboard</a></li>
-                        </ul>
-                    
-                </li>
-            </ul>
-
-            <button id="open_nav-btn" type="button"><i class="fa-solid fa-bars"></i></button>
-            <button id="close_nav-btn" type="button"><i class="fa-solid fa-xmark"></i></button>
-        </div>
-    </nav>
-
-
-<!-- ========== End of Nav ========== -->
