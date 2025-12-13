a5ff98c94b3a692b8ef69f7eb66dd0fa6fb2b8f0
Subrata Mahato <104990409+2004subrata@users.noreply.github.com>
2025-12-13 23:53:40 +0530

    Get back form data if invalid and show error message

--- admin/add-category.php
+++ admin/add-category.php
@@ -1,17 +1,28 @@
 <?php
 require 'partials/header.php';
+
+// get back form data if invalid
+$title = $_SESSION['add-category-data']['title'] ?? null;
+$description = $_SESSION['add-category-data']['description'] ?? null;
+
+unset($_SESSION['add-category-data']);
 ?>
 
 
 <section class="form_section">
     <div class="container form_section-container">
         <h2>Add Category</h2>
-        <div class="alert_message error">
-            <p>This is an error message</p>
-        </div>
+        <?php if(isset($_SESSION['add-category'])) :  ?>
+            <div class="alert_message error">
+                <p>
+                    <?= $_SESSION['add-category'];
+                    unset($_SESSION['add-category']) ?>
+                </p>
+            </div>
+        <?php endif ?>
         <form action="<?= ROOT_URl ?>admin/add-category-logic.php" method="post">
-            <input type="text" name="title" placeholder="Title">
-            <textarea rows="4" name="description" placeholder="Description"></textarea>
+            <input type="text" value="<?= $title ?>" name="title" placeholder="Title">
+            <textarea rows="4" name="description" placeholder="Description"><?= $description ?></textarea>
             <button type="submit" name="submit" class="btn">Add Category</button>
         </form>
     </div>
