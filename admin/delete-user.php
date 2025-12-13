2f5f0f15a6e0e53ff41a21e0c3ea40fb08006a0a
Subrata Mahato <104990409+2004subrata@users.noreply.github.com>
2025-12-13 22:04:53 +0530

    delete user from database

---
+++ admin/delete-user.php
@@ -0,0 +1,41 @@
+<?php
+require 'config/database.php';
+
+if(isset($_GET['id'])) {
+    $id = filter_var($_GET['id'], FILTER_SANITIZE_NUMBER_INT);
+
+    // fetch user from database
+    $query = "SELECT * FROM users WHERE id=$id";
+    $result = mysqli_query($connection, $query);
+    $user = mysqli_fetch_assoc($result);
+
+    // make sure we get back only one user
+    if(mysqli_num_rows($result) == 1) {
+        // var_dump($user);
+        $avatar_name = $user['avatar'];
+        $avatar_path ='../images/' .$avatar_name;
+        // delete image if available
+        if(file_exists($avatar_path)) {
+            unlink($avatar_path);
+        }
+    }
+
+    // FOR LATER
+    // fetch all thumnails of user's posts and delete them
+
+
+
+
+    // delete user from database
+    $delete_user_query = "DELETE FROM users WHERE id=$id";
+    $delete_user_result = mysqli_query($connection, $delete_user_query);
+    if(mysqli_error($connection)) {
+        $_SESSION['delete-user'] = "Couldn't delete '{$user['firstname']}' '{$user['lastname']}'";
+    }
+    else {
+        $_SESSION['delete-user-success'] = " '{$user['firstname']}' '{$user['lastname']}' delete successfully";
+    }
+}
+
+header('location: ' . ROOT_URl . 'admin/manage-users.php');
+die();
