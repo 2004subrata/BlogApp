dbdcb971184cff4e1295f9d52843c0b3f09bc38c
Subrata Mahato <104990409+2004subrata@users.noreply.github.com>
2025-12-15 20:30:01 +0530

    ADD LOGIC

--- admin/index.php
+++ admin/index.php
@@ -1,5 +1,10 @@
 <?php
 require 'partials/header.php';
+
+// fetch current user's posts from database
+$current_user_id = $_SESSION['user-id'];
+$query = "SELECT id, title, category_id FROM posts WHERE author_id=$current_user_id ORDER BY id DESC";
+$posts = mysqli_query($connection, $query);
 ?>
 
 
@@ -46,6 +51,7 @@ require 'partials/header.php';
         </aside>
         <main>
             <h2>Manage Posts</h2>
+            <?php if(mysqli_num_rows($posts) > 0) : ?>
             <table>
                 <thead>
                     <th>Title</th>
@@ -54,31 +60,25 @@ require 'partials/header.php';
                     <th>Delete</th>
                 </thead>
                 <tbody>
+                    <?php while($post = mysqli_fetch_assoc($posts)) : ?>
+                        <!-- get category title of each post from categories table -->
+                        <?php 
+                        $category_id = $post['category_id'];
+                        $category_query = "SELECT title FROM categories WHERE id=$category_id";
+                        $category_result = mysqli_query($connection, $category_query);
+                        $category = mysqli_fetch_assoc($category_result);
+                        ?>
                     <tr>
-                        <td>Lorem ipsum dolor sit amet consectetur adipisicing elit.</td>
-                        <td>Wild Life</td>
-                        <td><a href="edit-post.php" class="btn sm">Edit</a></td>
-                        <td><a href="delete-category.php" class="btn sm danger">Delete</a></td>
+                        <td><?= $post['title'] ?></td>
+                        <td><?= $category['title'] ?></td>
+                        <td><a href="<?= ROOT_URl ?>admin/edit-post.php?id=<?= $post['id'] ?>" class="btn sm">Edit</a></td>
+                        <td><a href="<?= ROOT_URl ?>admin/edit-post.php?id=<?= $post['id'] ?>" class="btn sm danger">Delete</a></td>
                     </tr>
-                    <tr>
-                        <td>Lorem ipsum dolor sit amet consectetur adipisicing elit.</td>
-                        <td>Wild Life</td>
-                        <td><a href="edit-post.php" class="btn sm">Edit</a></td>
-                        <td><a href="delete-category.php" class="btn sm danger">Delete</a></td>
-                    </tr>
-                    <tr>
-                        <td>Lorem ipsum dolor sit amet consectetur adipisicing elit.</td>
-                        <td>Wild Life</td>
-                        <td><a href="edit-post.php" class="btn sm">Edit</a></td>
-                        <td><a href="delete-category.php" class="btn sm danger">Delete</a></td>
-                    </tr>
-                    <tr>
-                        <td>Lorem ipsum dolor sit amet consectetur adipisicing elit.</td>
-                        <td>Wild Life</td>
-                        <td><a href="edit-post.php" class="btn sm">Edit</a></td>
-                        <td><a href="delete-category.php" class="btn sm danger">Delete</a></td>
-                    </tr>
+                    <?php endwhile ?>
                 </tbody>
+                <?php else : ?>
+                    <div class="alert_message error"><?= "No posts found" ?></div>
+                <?php endif ?>
             </table>
         </main>
     </div>
