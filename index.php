50c81dfae0e770a346ea7934df8cb0b077db8442
Subrata Mahato <104990409+2004subrata@users.noreply.github.com>
2025-12-26 20:31:30 +0530

    refactor index.php to dynamically display featured post and posts with improved structure and data fetching

--- index.php
+++ index.php
@@ -1,174 +1,107 @@
 <?php
 include 'partials/header.php';
 
+// fetch featured post from database
+$featured_query = "SELECT * FROM posts WHERE is_featured=1";
+$featured_result = mysqli_query($connection, $featured_query);
+$featured = mysqli_fetch_assoc($featured_result);
+
+// fetch 9 posts from posts table
+$query = "SELECT * FROM posts ORDER BY date_time DESC LIMIT 9";
+$posts = mysqli_query($connection, $query);
 ?>
 
+
+<!-- show featured post if there's any -->
+<?php if(mysqli_num_rows($featured_result) == 1) : ?>
 <section class="featured">
     <div class="container featured_container">
         <div class="post_thumbnail">
-            <img src="./images/blog1.jpg" alt="">
+            <img src="./images/<?= $featured['thumbnail'] ?>" alt="">
         </div>
         <div class="post_info">
-            <a href="category-post.html" class="category_button">wild Life</a>
-            <h2 class="post_title"><a href="post.html">Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta!</a></h2>
-            <p class="post_body">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Temporibus necessitatibus harum in voluptatum ullam, dicta nisi quidem similique adipisci repudiandae, ut mollitia veritatis ratione. Aliquam voluptatibus quia omnis id rem.</p>
+            <?php 
+            // fetch category from categories table using category_id of post
+            $category_id = $featured['category_id'];
+            $category_query = "SELECT * FROM categories WHERE id=$category_id";
+            $category_result = mysqli_query($connection, $category_query);
+            $category = mysqli_fetch_assoc($category_result);
+            ?>
+            <a href="<?= ROOT_URl ?>category-post.php?id=<?= $featured['category_id'] ?>" class="category_button"><?= $category['title'] ?></a>
+            <h2 class="post_title"><a href="<?= ROOT_URl ?>post.php?id=<?= $featured['id'] ?>"><?= $featured['title'] ?></a></h2>
+            <p class="post_body">
+                <!-- <?= substr($featured['body'], 0, 300)  ?>..... -->
+                <?= nl2br(htmlspecialchars(substr($featured['body'], 0, 150), )) ?>.....
+            </p>
             <div class="post_author">
+                <?php 
+                // fetch author from users table using author_id
+                $author_id = $featured['author_id'];
+                $author_query = "SELECT * FROM users WHERE id=$author_id";
+                $author_result = mysqli_query($connection, $author_query);
+                $author = mysqli_fetch_assoc($author_result);
+                ?>
                 <div class="post_author-avatar">
-                    <img src="./images/avatar2.jpg" alt="">
+                    <img src="./images/<?= $author['avatar'] ?>" alt="">
                 </div>
                 <div class="post_auther-info">
-                    <h5>By: Subrata Mahato</h5>
-                    <small>Nov 14, 2025 - 01:13</small>
+                    <h5>By: <?= "{$author['firstname']} {$author['lastname']}" ?></h5>
+                    <small><?= date("M d, Y - H:i", strtotime($featured['date_time'])) ?></small>
                 </div>
             </div>
         </div>
     </div>
 </section>
+<?php endif ?>
 
 
 <!-- ========== End of Featured ========== -->
 
 
-<section class="posts">
+<section class="posts <?= $featured ? '' : 'section_extra-margin' ?>">
     <div class="container posts_container">
+        <?php while($post = mysqli_fetch_assoc($posts)) : ?>
         <article class="post">
             <div class="post_thumbnail">
-                <img src="./images/blog2.jpg" alt="">
+                <img src="./images/<?= $post['thumbnail'] ?>" alt="">
             </div>
             <div class="post_info">
-                <a href="category-post.html" class="category_button">Wilf Life</a>
+                <?php 
+                // fetch category from categories table using category_id of post
+                $category_id = $post['category_id'];
+                $category_query = "SELECT * FROM categories WHERE id=$category_id";
+                $category_result = mysqli_query($connection, $category_query);
+                $category = mysqli_fetch_assoc($category_result);
+                ?>
+                <a href="<?= ROOT_URl ?>category-post.php?id=<?= $post['category_id'] ?>" class="category_button"><?= $category['title'] ?></a>
                 <h3 class="post_title">
-                    <a href="post.html">Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, aliquid.</a>
+                    <a href="<?= ROOT_URl ?>post.php?id=<?= $post['id'] ?>"><?= $post['title'] ?></a>
                 </h3>
                 <p class="post_body">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus id, ipsam aperiam sint doloribus tenetur iusto? Qui voluptas, perferendis vitae magni dicta cumque.
+                    <?= substr($post['body'], 0, 150) ?>...
+                    <!-- <?= nl2br(htmlspecialchars(substr($post['body'], 0, 150), )) ?>..... -->
                 </p>
                 <div class="post_author">
+                    <?php 
+                    // fetch author from users table using author_id
+                    $author_id = $post['author_id'];
+                    $author_query = "SELECT * FROM users WHERE id=$author_id";
+                    $author_result = mysqli_query($connection, $author_query);
+                    $author = mysqli_fetch_assoc($author_result);
+                    ?>
                     <div class="post_author-avatar">
-                        <img src="./images/avatar3.jpg" alt="">
+                        <img src="./images/<?= $author['avatar'] ?>" alt="">
                     </div>
                     <div class="post_author-info">
-                        <h5>By: Bappa Bauri</h5>
-                        <small>Nov 17, 2025 - 23:22</small>
+                        <h5>By: <?= "{$author['firstname']} {$author['lastname']}" ?></h5>
+                        <small>
+                            <?= date("M d, Y - H:i", strtotime($post['date_time'])) ?>
+                        </small>
                     </div>
                 </div>
             </div>
         </article>
-        <article class="post">
-            <div class="post_thumbnail">
-                <img src="./images/blog3.jpg" alt="">
-            </div>
-            <div class="post_info">
-                <a href="" class="category_button">Wilf Life</a>
-                <h3 class="post_title">
-                    <a href="post.html">Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, aliquid.</a>
-                </h3>
-                <p class="post_body">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus id, ipsam aperiam sint doloribus tenetur iusto? Qui voluptas, perferendis vitae magni dicta cumque.
-                </p>
-                <div class="post_author">
-                    <div class="post_author-avatar">
-                        <img src="./images/avatar4.jpg" alt="">
-                    </div>
-                    <div class="post_author-info">
-                        <h5>By: Bappa Bauri</h5>
-                        <small>Nov 17, 2025 - 23:22</small>
-                    </div>
-                </div>
-            </div>
-        </article>
-        <article class="post">
-            <div class="post_thumbnail">
-                <img src="./images/blog4.jpg" alt="">
-            </div>
-            <div class="post_info">
-                <a href="" class="category_button">Wilf Life</a>
-                <h3 class="post_title">
-                    <a href="post.html">Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, aliquid.</a>
-                </h3>
-                <p class="post_body">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus id, ipsam aperiam sint doloribus tenetur iusto? Qui voluptas, perferendis vitae magni dicta cumque.
-                </p>
-                <div class="post_author">
-                    <div class="post_author-avatar">
-                        <img src="./images/avatar5.jpg" alt="">
-                    </div>
-                    <div class="post_author-info">
-                        <h5>By: Bappa Bauri</h5>
-                        <small>Nov 17, 2025 - 23:22</small>
-                    </div>
-                </div>
-            </div>
-        </article>
-        <article class="post">
-            <div class="post_thumbnail">
-                <img src="./images/blog5.jpg" alt="">
-            </div>
-            <div class="post_info">
-                <a href="" class="category_button">Wilf Life</a>
-                <h3 class="post_title">
-                    <a href="post.html">Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, aliquid.</a>
-                </h3>
-                <p class="post_body">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus id, ipsam aperiam sint doloribus tenetur iusto? Qui voluptas, perferendis vitae magni dicta cumque.
-                </p>
-                <div class="post_author">
-                    <div class="post_author-avatar">
-                        <img src="./images/avatar6.jpg" alt="">
-                    </div>
-                    <div class="post_author-info">
-                        <h5>By: Bappa Bauri</h5>
-                        <small>Nov 17, 2025 - 23:22</small>
-                    </div>
-                </div>
-            </div>
-        </article>
-        <article class="post">
-            <div class="post_thumbnail">
-                <img src="./images/blog6.jpg" alt="">
-            </div>
-            <div class="post_info">
-                <a href="" class="category_button">Wilf Life</a>
-                <h3 class="post_title">
-                    <a href="post.html">Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, aliquid.</a>
-                </h3>
-                <p class="post_body">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus id, ipsam aperiam sint doloribus tenetur iusto? Qui voluptas, perferendis vitae magni dicta cumque.
-                </p>
-                <div class="post_author">
-                    <div class="post_author-avatar">
-                        <img src="./images/avatar7.jpg" alt="">
-                    </div>
-                    <div class="post_author-info">
-                        <h5>By: Bappa Bauri</h5>
-                        <small>Nov 17, 2025 - 23:22</small>
-                    </div>
-                </div>
-            </div>
-        </article>
-        <article class="post">
-            <div class="post_thumbnail">
-                <img src="./images/blog7.jpg" alt="">
-            </div>
-            <div class="post_info">
-                <a href="" class="category_button">Wilf Life</a>
-                <h3 class="post_title">
-                    <a href="post.html">Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, aliquid.</a>
-                </h3>
-                <p class="post_body">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus id, ipsam aperiam sint doloribus tenetur iusto? Qui voluptas, perferendis vitae magni dicta cumque.
-                </p>
-                <div class="post_author">
-                    <div class="post_author-avatar">
-                        <img src="./images/avatar8.jpg" alt="">
-                    </div>
-                    <div class="post_author-info">
-                        <h5>By: Bappa Bauri</h5>
-                        <small>Nov 17, 2025 - 23:22</small>
-                    </div>
-                </div>
-            </div>
-        </article>
+        <?php endwhile ?>
     </div>
 </section>
 
@@ -178,12 +111,13 @@ include 'partials/header.php';
 
 <section class="category_buttons">
     <div class="container category_buttons-container">
-        <a href="" class="category_button">Art </a>
-        <a href="" class="category_button">Wild Life</a>
-        <a href="" class="category_button">Travel</a>
-        <a href="" class="category_button">Science & Technology</a>
-        <a href="" class="category_button">Food</a>
-        <a href="" class="category_button">Music</a>
+        <?php 
+        $all_categories_query = "SELECT * FROM categories";
+        $all_categories = mysqli_query($connection, $all_categories_query);
+        ?>
+        <?php while($category = mysqli_fetch_assoc($all_categories)) : ?>
+            <a href="<?= ROOT_URl ?>category-post.php?id=<?= $category['id'] ?>" class="category_button"><?= $category['title'] ?> </a>
+        <?php endwhile ?>
     </div>
 </section>
 
